f7e4f7ffec56d459a4014a53687e623a5bd3a6e0
BostonWafro <wafrogaming@gmail.com>
2021-02-03 14:04:54 +0000

    More styling

--- src/login/index.js
+++ src/login/index.js
@@ -3,13 +3,11 @@ import './index.css';
 
 const LoginBox = (props) => {
     return(
-        <div className='loginBox'>
-            <form name='login' className='login' onSubmit={props.handleSubmit}>
-                <input className='inputBox' type='text' name='userName' value={props.userVal} onChange={props.onChange}/>
-                <input className='inputBox' type='text' name='password' value={props.passVal} onChange={props.onChange}/>
-                <button className='submitButton' type='submit' disabled={props.isDisabled}/>
-            </form>
-        </div>
+        <form name='login' className='login' onSubmit={props.handleSubmit}>
+            <input className='inputBox' type='text' name='userName' value={props.userVal} onChange={props.onChange}/>
+            <input className='inputBox' type='text' name='password' value={props.passVal} onChange={props.onChange}/>
+            <button className='submitButton' type='submit' disabled={props.isDisabled}/>
+        </form>
     );
 };
 
