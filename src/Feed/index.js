f6b3c39d0dd09bee9acd9fa9e3e794c8b1435b01
BostonWafro <wafrogaming@gmail.com>
2021-02-03 13:33:30 +0000

    started css

--- src/Feed/index.js
+++ src/Feed/index.js
@@ -1,3 +1,4 @@
+import './index.css';
 
 const Feed = (props) => {
     if (props.isLoaded) {
--- src/login/index.js
+++ src/login/index.js
@@ -4,7 +4,7 @@ import './index.css';
 const LoginBox = (props) => {
     return(
         <div className='loginBox'>
-            <form name='login' onSubmit={props.handleSubmit}>
+            <form name='login' className='login' onSubmit={props.handleSubmit}>
                 <input className='inputBox' type='text' name='userName' value={props.userVal} onChange={props.onChange}/>
                 <input className='inputBox' type='text' name='password' value={props.passVal} onChange={props.onChange}/>
                 <button className='submitButton' type='submit' disabled={props.isDisabled}/>
